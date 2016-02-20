e4d85975e1394c27f77d91b355ffef08d36958b2
syuchan1005 <@syu_chan_1005>
2016-02-20 16:43:15 +0900

    Inventoryのシリアライズ, デシリアライズメソッドを追加

---
+++ src/main/java/co/honobono/honobonoserver/util/InventoryUtil.java
@@ -0,0 +1,36 @@
+package co.honobono.honobonoserver.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
+
+/**
+ * Created by syuchan on 2016/02/20.
+ */
+public class InventoryUtil {
+
+	public static String serialize(Inventory inventory) throws IOException {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+		dataOutput.writeInt(inventory.getSize());
+		for (int i = 0; i < inventory.getSize(); i++) 	dataOutput.writeObject(inventory.getItem(i));
+		dataOutput.close();
+		return Base64Coder.encodeLines(outputStream.toByteArray());
+	}
+
+	public static Inventory deserialize(String data) throws IOException, ClassNotFoundException {
+		ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
+		BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
+		Inventory inventory = Bukkit.getServer().createInventory(null, dataInput.readInt());
+		for (int i = 0; i < inventory.getSize(); i++) inventory.setItem(i, (ItemStack) dataInput.readObject());
+		dataInput.close();
+		return inventory;
+	}
+}
