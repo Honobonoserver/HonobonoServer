53dd4664949fb72df5342061a19e2414539b87fd
syuchan1005 <@syu_chan_1005>
2016-02-20 16:46:13 +0900

    Location#toStringした値をLocationに戻すmethodを追加

--- src/main/java/co/honobono/honobonoserver/util/Other.java
+++ src/main/java/co/honobono/honobonoserver/util/Other.java
@@ -7,11 +7,15 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.Plugin;
 
 public class Other {
@@ -29,4 +33,17 @@ public class Other {
 		conf.load(reader);
 		return conf;
 	}
+
+	public static Location toLocation(String s) {
+		String[] loc = s.split(",");
+		World w = Bukkit.getWorld(loc[0].substring(31, loc[0].length() - 1));
+		double x = Double.valueOf(loc[1].substring(2, loc[1].length()));
+		double y = Double.valueOf(loc[2].substring(2, loc[2].length()));
+		double z = Double.valueOf(loc[3].substring(2, loc[3].length()));
+		float pitch = Float.valueOf(loc[4].substring(6, loc[4].length()));
+		float yaw = Float.valueOf(loc[5].substring(4, loc[5].length() - 1));
+		Location loc1 = new Location(w, x, y, z, yaw, pitch);
+		return loc1;
+	}
+
 }
