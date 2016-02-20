373946191abede1c6d4b0de3ab64ddbecbbbcd44
syuchan1005 <@syu_chan_1005>
2016-02-20 16:49:57 +0900

    細かい修正

--- src/main/java/co/honobono/honobonoserver/HonobonoServer.java
+++ src/main/java/co/honobono/honobonoserver/HonobonoServer.java
@@ -3,11 +3,15 @@ package co.honobono.honobonoserver;
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.LinkedHashMap;
+import java.util.*;
 
+import co.honobono.honobonoserver.command.PocketChestCommand;
 import co.honobono.honobonoserver.constructor.LanguageHelper;
+import co.honobono.honobonoserver.util.InventoryUtil;
+import com.sun.javafx.collections.MappingChange;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -19,31 +23,28 @@ import co.honobono.honobonoserver.runnable.WitherMoveRunnable;
 import co.honobono.honobonoserver.util.Other;
 
 public class HonobonoServer extends JavaPlugin {
-	private static Plugin plugin;
-	private static RegistManager manager;
-	private static FileConfiguration config;
-	private static LanguageHelper langs;
-	private static SQLite SQL;
-	private static LinkedHashMap<String, Casts[]> map = new LinkedHashMap<>();{
-		map.put("PlayerUUID", Casts.toArray(Casts.TEXT, Casts.NOTNULL, Casts.UNIQUE));
-		map.put("PlayerName", Casts.toArray(Casts.TEXT, Casts.NOTNULL));
-		map.put("Time", Casts.toArray(Casts.TIMESTAMP));
-		map.put("Home-World", Casts.toArray(Casts.TEXT));
-		map.put("Home-X", Casts.toArray(Casts.INTEGER));
-		map.put("Home-Y", Casts.toArray(Casts.INTEGER));
-		map.put("Home-Z", Casts.toArray(Casts.INTEGER));
-	}
+	private RegistManager manager;
+	private FileConfiguration config;
+	private FileConfiguration inv;
+	private LanguageHelper langs;
+	private SQLite SQL;
 
 	@Override
 	public void onEnable() {
 		try {
-			plugin = this;
-			config = Other.getConfig(this, "config.yml");
-			langs = new LanguageHelper(this);
-			getSQLite();
-			SQL.create("Home", map);
-			manager = new RegistManager(this);
-			manager.registerCommand("hn", "HonobonoServer's Plugin Command.", "/hn(h) <options>", "honobonoserver.command", "You don't have Permission.", "h");
+			this.config = Other.getConfig(this, "config.yml");
+			this.inv = Other.getConfig(this, "pocketchest.yml");
+			this.langs = new LanguageHelper(this);
+			this.SQL = new SQLite(new File(this.getDataFolder(), "Setting.db"));
+			this.SQL.add("PlayerUUID", Casts.TEXT, Casts.NOTNULL, Casts.UNIQUE)
+					.add("PlayerName", Casts.TEXT, Casts.NOTNULL)
+					.add("Time", Casts.TIMESTAMP)
+					.add("Home-World", Casts.INTEGER)
+					.add("Home-X", Casts.INTEGER)
+					.add("Home-Y", Casts.INTEGER)
+					.add("Home-Z", Casts.INTEGER).create("Home");
+			this.manager = new RegistManager(this);
+			this.manager.registerCommand("hn", "HonobonoServer's Plugin Command.", "/hn(h) <options>", "honobonoserver.command", "You don't have Permission.", "h");
 		} catch (ReflectiveOperationException | IOException | SQLException | InvalidConfigurationException e) {
 			e.printStackTrace();
 		}
@@ -51,27 +52,40 @@ public class HonobonoServer extends JavaPlugin {
 		new WitherMoveRunnable().runTaskTimer(this, 0, 5);
 	}
 
-	public static FileConfiguration getConfigFile() {
-		return config;
-	}
-
-	public static SQLite getSQLite() {
-		if (SQL == null) {
+	@Override
+	public void onDisable() {
+		Map<String, Inventory> map = PocketChestCommand.getMap();
+		if(map != null) {
+			Map<String, String> a = new HashMap<>();
 			try {
-				SQL = new SQLite(new File(plugin.getDataFolder(), "Setting.db"));
-			} catch (ClassNotFoundException | SQLException e) {
+				for(Map.Entry<String, Inventory> entry : map.entrySet()) {
+					a.put(entry.getKey(), InventoryUtil.serialize(entry.getValue()));
+				}
+				this.getInv().set("Inventories", Arrays.asList(a));
+				this.getInv().save(new File(this.getDataFolder(), "pocketchest.yml"));
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		return SQL;
 	}
 
-	public static RegistManager getManager() {
-		return manager;
+	public FileConfiguration getConfigFile() {
+		return this.config;
 	}
 
-	public static LanguageHelper getLanguages() {
-		return langs;
+	public SQLite getSQLite() {
+		return this.SQL;
 	}
 
+	public RegistManager getManager() {
+		return this.manager;
+	}
+
+	public LanguageHelper getLanguages() {
+		return this.langs;
+	}
+
+	public FileConfiguration getInv() {
+		return this.inv;
+	}
 }
