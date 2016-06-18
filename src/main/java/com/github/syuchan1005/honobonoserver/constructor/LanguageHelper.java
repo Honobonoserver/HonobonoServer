65e98d0443ea1409d9b47b23e7aa7715deba1b54
syuchan1005@syu_chan_1005 <syuchan.dev@gmail.com>
2016-06-18 16:43:41 +0900

    LanguageHalperのインデント等修正

--- src/main/java/com/github/syuchan1005/honobonoserver/constructor/LanguageHelper.java
+++ src/main/java/com/github/syuchan1005/honobonoserver/constructor/LanguageHelper.java
@@ -1,14 +1,5 @@
 package com.github.syuchan1005.honobonoserver.constructor;
 
-import java.io.*;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.stream.Collectors;
-
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.InvalidConfigurationException;
@@ -17,31 +8,45 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
+
 public class LanguageHelper {
 	private Map<String, FileConfiguration> Configs = new HashMap<>();
 	private String defaultLang = null;
 
 	public LanguageHelper(Plugin plugin) throws IOException, InvalidConfigurationException {
 		FileConfiguration f = getConfig(plugin, "config.yml");
-		if(f.isString("DefaultLanguage")) defaultLang = f.getString("DefaultLanguage");
+		if (f.isString("DefaultLanguage")) defaultLang = f.getString("DefaultLanguage");
 		JarFile jar = null;
 		try {
 			jar = new JarFile(plugin.getClass().getProtectionDomain().getCodeSource().getLocation().getFile());
-			for (Enumeration<JarEntry> e = jar.entries(); e.hasMoreElements();) {
+			for (Enumeration<JarEntry> e = jar.entries(); e.hasMoreElements(); ) {
 				String Name = e.nextElement().getName();
 				if (!Name.endsWith(".lang")) continue;
 				String[] name = Name.split("/");
 				Configs.put(name[name.length - 1].replaceAll(".lang", ""), getConfig(plugin, Name));
 			}
 		} finally {
-			if(jar != null) jar.close();
+			if (jar != null) jar.close();
 		}
 	}
 
 	public String getString(CommandSender sender, String path) {
-		if(sender instanceof Player) {
-			return this.getString((Player)sender, path);
-		}
+		if (sender instanceof Player) return this.getString((Player) sender, path);
 		return (defaultLang == null) ? path : this.getString(defaultLang, path);
 	}
 
@@ -54,16 +59,12 @@ public class LanguageHelper {
 	}
 
 	public String getString(String LangName, String path) {
-		if(Configs.containsKey(LangName)) {
-			return toColor(Configs.get(LangName).getString(path));
-		}
+		if (Configs.containsKey(LangName)) return toColor(Configs.get(LangName).getString(path));
 		return (defaultLang == null) ? path : this.getString(defaultLang, path);
 	}
 
 	public List<String> getStringList(CommandSender sender, String path) {
-		if(sender instanceof Player) {
-			return this.getStringList((Player)sender, path);
-		}
+		if (sender instanceof Player) return this.getStringList((Player) sender, path);
 		return (defaultLang == null) ? Arrays.asList(path) : this.getStringList(defaultLang, path);
 	}
 
@@ -76,32 +77,32 @@ public class LanguageHelper {
 	}
 
 	public List<String> getStringList(String LangName, String path) {
-		if(Configs.containsKey(LangName)) {
+		if (Configs.containsKey(LangName))
 			return Configs.get(LangName).getStringList(path).stream().map(s -> toColor(s)).collect(Collectors.toList());
-		}
 		return (defaultLang == null) ? Arrays.asList(path) : this.getStringList(defaultLang, path);
 	}
 
 	public FileConfiguration getConfig(Plugin plugin, String filename) throws IOException, InvalidConfigurationException {
 		File file = new File(plugin.getDataFolder(), filename);
 		if (!file.exists()) plugin.saveResource(filename, false);
-		Reader reader = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8);
+		Reader reader = new InputStreamReader(new FileInputStream(file), "UTF-8");
 		FileConfiguration conf = new YamlConfiguration();
 		conf.load(reader);
 		return conf;
 	}
 
 	private static Method handleMethod = null;
-    private static Field localeField = null;
+	private static Field localeField = null;
+
 	public static String getLocale(Player player) throws ReflectiveOperationException {
-        if (handleMethod == null) handleMethod = player.getClass().getDeclaredMethod("getHandle");
+		if (handleMethod == null) handleMethod = player.getClass().getDeclaredMethod("getHandle");
 		Object o = handleMethod.invoke(player);
-		if(localeField == null) localeField = o.getClass().getField("locale");
-		return (String)localeField.get(o);
+		if (localeField == null) localeField = o.getClass().getField("locale");
+		return (String) localeField.get(o);
 	}
 
 	private static String toColor(String msg) {
-		if(msg == null) return "null";
+		if (msg == null) return "null";
 		return ChatColor.translateAlternateColorCodes('&', msg);
 	}
 }
